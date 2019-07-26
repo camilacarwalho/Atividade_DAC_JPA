8efbd0aa3122608e729f8d5e5adaebffeadd2225
antonio <antoniofilhonerde@gmail.com>
2019-07-26 08:45:30 -0300

    relacionamento livro e autor

--- jpa-consultas/src/main/java/domain/Autor.java
+++ jpa-consultas/src/main/java/domain/Autor.java
@@ -1,21 +1,33 @@
 package domain;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Autor extends Pessoa implements Serializable  {
+
     private String instituicaoVInculada;
+
+    @ManyToMany
+    @JoinTable(name="autorLivro",
+            joinColumns = @JoinColumn(name = "autor_id"),
+             inverseJoinColumns = @JoinColumn(name = "livro_id") )
+    private List<Livro> livros;
 
     public Autor(){
 
     }
 
-    public Autor(String nome, String cpf, int idade, Date dataNascimento, Endereco endereco, String instituicaoVInculada) {
+    public Autor(String nome, String cpf, int idade, Date dataNascimento, Endereco endereco, String instituicaoVInculada, List<Livro> livros) {
         super(nome, cpf, idade, dataNascimento, endereco);
         this.instituicaoVInculada = instituicaoVInculada;
+        this.livros = livros;
     }
 
 
@@ -25,5 +37,13 @@ public class Autor extends Pessoa implements Serializable  {
 
     public void setInstituicaoVInculada(String instituicaoVInculada) {
         this.instituicaoVInculada = instituicaoVInculada;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
     }
 }
--- jpa-consultas/src/main/java/domain/Livro.java
+++ jpa-consultas/src/main/java/domain/Livro.java
@@ -1,7 +1,9 @@
 package domain;
 
+import javax.persistence.ManyToMany;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 public class Livro implements Serializable {
 
@@ -9,7 +11,48 @@ public class Livro implements Serializable {
     private String nome;
     private Date lançamento;
 
+    @ManyToMany(mappedBy = "livros")
+    private List<Autor> autores;
 
+    public Livro() {
+    }
 
+    public Livro(String ISBN, String nome, Date lançamento, List<Autor> autores) {
+        this.ISBN = ISBN;
+        this.nome = nome;
+        this.lançamento = lançamento;
+        this.autores = autores;
+    }
 
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getLançamento() {
+        return lançamento;
+    }
+
+    public void setLançamento(Date lançamento) {
+        this.lançamento = lançamento;
+    }
+
+    public List<Autor> getAutores() {
+        return autores;
+    }
+
+    public void setAutores(List<Autor> autores) {
+        this.autores = autores;
+    }
 }
