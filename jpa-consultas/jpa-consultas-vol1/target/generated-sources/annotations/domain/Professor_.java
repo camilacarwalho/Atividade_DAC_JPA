0a322d73331d3baaf47a82c8e0e36969cb6f5e14
antonio <antoniofilhonerde@gmail.com>
2019-07-29 10:05:50 -0300

    correcao erro persistence

--- jpa-consultas/jpa-consultas-vol1/target/generated-sources/annotations/domain/Professor_.java
+++ jpa-consultas/jpa-consultas-vol1/target/generated-sources/annotations/domain/Professor_.java
@@ -2,15 +2,15 @@ package domain;
 
 import domain.Telefone;
 import javax.annotation.Generated;
-import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-07-26T16:37:37")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-07-29T10:03:57")
 @StaticMetamodel(Professor.class)
 public class Professor_ extends Pessoa_ {
 
     public static volatile SingularAttribute<Professor, Double> salario;
-    public static volatile SetAttribute<Professor, Telefone> telefones;
+    public static volatile ListAttribute<Professor, Telefone> telefones;
 
 }
--- jpa-consultas/jpa-consultas-vol1/target/generated-sources/annotations/domain/Telefone_.java
+++ jpa-consultas/jpa-consultas-vol1/target/generated-sources/annotations/domain/Telefone_.java
@@ -5,12 +5,11 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-07-26T16:37:37")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-07-29T10:03:57")
 @StaticMetamodel(Telefone.class)
 public class Telefone_ { 
 
     public static volatile SingularAttribute<Telefone, TelefoneType> tipo;
     public static volatile SingularAttribute<Telefone, String> numero;
-    public static volatile SingularAttribute<Telefone, Integer> id;
 
 }
