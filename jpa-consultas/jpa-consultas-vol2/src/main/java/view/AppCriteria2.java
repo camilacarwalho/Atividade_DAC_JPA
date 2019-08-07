304408abede9207fa9ac2859e08a82e113aa1620
camilacarwalho <camilacarwalho3@gmail.com>
2019-08-07 20:54:10 -0300

    AppCriteria2

--- jpa-consultas/jpa-consultas-vol2/src/main/java/view/AppCriteria2.java
+++ jpa-consultas/jpa-consultas-vol2/src/main/java/view/AppCriteria2.java
@@ -1,7 +1,13 @@
 package view;
 
+import domain.Area;
+import domain.Escritor;
+import domain.Publicacao;
+import domain.Revisor;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.*;
 
 public class AppCriteria2 {
     public static void main(String[] args) {
@@ -20,19 +26,75 @@ public class AppCriteria2 {
     //O nome da pessoa, o título da publicação e o nome da área em que a pessoa tem o
     //atributo id igual a 3.
     private static void letraA(EntityManager em) {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteria = builder.createQuery(Object[].class);
+        Root<Escritor> root = criteria.from(Escritor.class);
+
+        Join<Escritor, Publicacao> escritorPublicacaoJoin = root.join("publicacoes");
+        Join<Publicacao, Area> publicacaoAreaJoin = escritorPublicacaoJoin.join("areas");
+
+        Predicate id = builder.equal(root.get("id"),3);
+
+        criteria.where(id).multiselect(root.get("nome"),escritorPublicacaoJoin.get("titulo"),
+                publicacaoAreaJoin.get("nome"));
+
+        em.createQuery(criteria).getResultList().forEach(
+                p-> System.out.println("Nome: "+p[0]+"\nPublicação: "+p[1]+"\nArea: "+p[2]+"\n")
+        );
     }
 
     //O título da publicação e o nome do revisor que tenham alguma publicação na área
     //de indústria.
     private static void letraB(EntityManager em) {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteria = builder.createQuery(Object[].class);
+        Root<Revisor> root = criteria.from(Revisor.class);
+
+        Join<Revisor,Publicacao> revisorPublicacaoJoin = root.join("publicacoes");
+        Join<Publicacao, Area> publicacaoAreaJoin = revisorPublicacaoJoin.join("areas");
+
+        Predicate area = builder.like(builder.lower(publicacaoAreaJoin.get("nome")),"%industria%");
+
+        criteria.multiselect(revisorPublicacaoJoin.get("titulo"),
+                root.get("nome"))
+                .where(area);
+        em.createQuery(criteria).getResultList().forEach(
+                p-> System.out.println("Publicação: "+p[0]+"\nRevisor: "+p[1]+"\n")
+        );
     }
 
     //O nome dos Revisores que possuem alguma publicação começando com Java.
     private static void letraC(EntityManager em) {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<String> criteria = builder.createQuery(String.class);
+        Root<Revisor> root = criteria.from(Revisor.class);
+        Join<Revisor, Publicacao> join = root.join("publicacoes");
+        Predicate predicate = builder.like(builder.lower(join.get("titulo")),"java%");
+        criteria.where(predicate).select(root.get("nome"));
+        em.createQuery(criteria).getResultList().forEach(
+                System.out::println
+        );
+
     }
 
     //O nome e a quantidade de Publicações escritas por Escritores com mais que três
     //prêmios.
     private static void letraD(EntityManager em) {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteria = builder.createQuery(Object[].class);
+        Root<Escritor> root = criteria.from(Escritor.class);
+        Join<Escritor,Publicacao> join = root.join("publicacoes");
+
+        Predicate maiorQue3 = builder.gt(root.get("premios"),3);
+
+        criteria.multiselect(root.get("nome"),builder.count(join))
+                .where(maiorQue3)
+                .groupBy(root.get("nome"));
+
+        em.createQuery(criteria).getResultList().forEach(
+                e-> System.out.println("Escritor: "+e[0]+" com "+e[1]+" publicação(ões)")
+        );
+
+
     }
 }
