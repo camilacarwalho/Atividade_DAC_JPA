9e87dd6cdd85a2ce19fbfc5adb732eb665f1c64d
camilacarwalho <camilacarwalho3@gmail.com>
2019-08-11 22:03:30 -0300

    Arrumando erros jpql

--- jpa-consultas/jpa-consultas-vol2/src/main/java/view/AppJPQL2.java
+++ jpa-consultas/jpa-consultas-vol2/src/main/java/view/AppJPQL2.java
@@ -18,9 +18,12 @@ public class AppJPQL2 {
     //O nome da pessoa, o título da publicação e o nome da área em que a pessoa tem o
     //atributo id igual a 3.
     private static void letraA(EntityManager em) {
-        String jpql="SELECT e.nome, pu.titulo, a.nome FROM Escritor e, " +
-                "IN(e.publicacoes) pu, IN(pu.areas) a WHERE e.id=3";
+        //consulta jpql
+        String jpql="SELECT esc.nome, pu.titulo, a.nome FROM Escritor esc, " +
+                "IN(esc.publicacoes) pu, IN(pu.areas) a WHERE esc.id=3";
+
         TypedQuery<Object[]> query = em.createQuery(jpql,Object[].class);
+        //listando de acordo com a consulta
         query.getResultList().forEach(
                 p-> System.out.println("nome da pessoa: "+p[0]+"\n título da publicação: "+p[1]+"\n área: "+p[2]+"\n")
         );
@@ -29,7 +32,8 @@ public class AppJPQL2 {
     //O título da publicação e o nome do revisor que tenham alguma publicação na área
     //de indústria.
     private static void letraB(EntityManager em) {
-        String jpql="SELECT p.titulo, r.nome FROM Revisor r, IN (r.publicacoes) p,IN(p.areas) a " +
+        //consulta jpql selecionando titulo da publicação e nome do revisor
+        String jpql="SELECT p.titulo, rev.nome FROM Revisor rev, IN (rev.publicacoes) p,IN(p.areas) a " +
                 "WHERE LOWER(a.nome) like '%industria%' ";
         TypedQuery<Object[]> query = em.createQuery(jpql,Object[].class);
         query.getResultList().forEach(
@@ -39,7 +43,7 @@ public class AppJPQL2 {
 
     //O nome dos Revisores que possuem alguma publicação começando com Java.
     private static void letraC(EntityManager em) {
-        String jpql="SELECT r.nome FROM Revisor r, IN(r.publicacoes) p WHERE LOWER(p.titulo) LIKE 'java%'";
+        String jpql="SELECT rev.nome FROM Revisor rev, IN(rev.publicacoes) p WHERE LOWER(p.titulo) LIKE 'java%'";
         TypedQuery<String> query = em.createQuery(jpql, String.class);
         query.getResultList().forEach(System.out::println);
     }
@@ -47,8 +51,8 @@ public class AppJPQL2 {
     //O nome e a quantidade de Publicações escritas por Escritores com mais que três
     //prêmios.
     private static void letraD(EntityManager em) {
-        String jpql = "SELECT e.nome, count(p) FROM Escritor e, in(e.publicacoes) p " +
-                "WHERE e.premios>3 GROUP BY e.nome";
+        String jpql = "SELECT esc.nome, count(p) FROM Escritor esc, in(esc.publicacoes) p " +
+                "WHERE esc.premios>3 GROUP BY esc.nome";
         TypedQuery<Object[]> query = em.createQuery(jpql, Object[].class);
         query.getResultList().forEach(
                 e-> System.out.println("nome do escritor: "+e[0]+" quantidade de publicações: "+e[1])
